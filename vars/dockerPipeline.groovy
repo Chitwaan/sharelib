386330bc6ad93abfbec8697ee614dc810abbc5a7
Chitwaan <kaurchitwan7@gmail.com>
2024-04-11 13:38:31 -0700

    updated groovy file

--- vars/dockerPipeline.groovy
+++ vars/dockerPipeline.groovy
@@ -1,12 +1,10 @@
-// This is assuming you're creating a shared library global variable named 'dockerPipeline'
-// This script should be placed in the 'vars' directory of your shared library repository.
+
 
 def call(String dockerRepoName) {
     def serviceDir = [
         'receiver': 'Receiver',
         'storage': 'Storage',
         'processing': 'Processing'
-        // Add other services with the correct case as needed
     ]
     pipeline {
         agent any
@@ -31,7 +29,6 @@ def call(String dockerRepoName) {
             stage('Lint') {
                 steps {
                     script {
-                        // Assuming you are in the root directory of the checked out repository
                         if (sh(returnStatus: true, script: 'test -d ${serviceDir[params.SERVICE_NAME]}')) {
                             sh "pylint --fail-under=5 ${serviceDir[params.SERVICE_NAME]}/*.py"
                         } else {
@@ -63,11 +60,9 @@ def call(String dockerRepoName) {
                         script {
                             def serviceName = params.SERVICE_NAME.toLowerCase()
                             def dockerfilePath = "${serviceDir[serviceName]}/Dockerfile"
-                            // Change directory to the service directory before building.
-                            // This sets the build context to the service directory, which should contain both the Dockerfile and requirements.txt.
+                           
                             dir("${serviceDir[serviceName]}") {
                                 sh 'echo $TOKEN | docker login --username chitwankaur --password-stdin'
-                                // Since we're now in the correct directory, the build context is '.', and Dockerfile path doesn't need to be specified if named 'Dockerfile'.
                                 sh "docker build -t chitwankaur/mydockerrepo:${BUILD_NUMBER} ."
                                 sh "docker push chitwankaur/mydockerrepo:${BUILD_NUMBER}"
                             }
@@ -85,8 +80,7 @@ def call(String dockerRepoName) {
                 steps {
                     script {
                         sshagent(['deployment']) {
-                            // Ensure the SERVICE_NAME parameter matches one of the service names in docker-compose.yml
-                            def deployService = params.SERVICE_NAME // e.g., 'receiver'
+                            def deployService = params.SERVICE_NAME 
                             sh "ssh -o StrictHostKeyChecking=no chitwan@40.76.138.76 'cd Microservices/Deployment && docker compose pull ${deployService} && docker compose up -d ${deployService}'"
                         }
                     }
